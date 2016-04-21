cf4858abc69667b38a61c6dd74d4365fd64009dc
chepe11295 <josemarroquin_10@hotmail.com>
2016-04-21 00:04:46 -0600

    Update puntero_a_estructura.cpp

--- puntero_a_estructura/src/puntero_a_estructura.cpp
+++ puntero_a_estructura/src/puntero_a_estructura.cpp
@@ -22,12 +22,12 @@ ef.nombre = "Jalapa";
 ef.integrantes = 17;
 ef.posicion = 1;
 
-Equipo *punstruct;
-punstruct = &ef;
+Equipo *puntero;
+puntero = &ef;
 
-cout<<"la posicion de la estructura ef es                      : "<<&punstruct<<endl;
-cout<<"la posicion del nombre del equipo es                    : "<<&ef.nombre<<endl;
-cout<<"la posicion del numero de integrantes del del equipo es : "<<&ef.integrantes<<endl;
-cout<<"la posicion de la posicion del equipo es                : "<<&ef.posicion<<endl;
+cout<<"la posicion de la estructura ef es                      : "<<puntero<<endl;
+cout<<"la posicion del nombre del equipo es                    : "<<&puntero->nombre<<endl;
+cout<<"la posicion del numero de integrantes del del equipo es : "<<&puntero->integrantes<<endl;
+cout<<"la posicion de la posicion del equipo es                : "<<&puntero->posicion<<endl;
 
 }
